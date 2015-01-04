377e38de046fd1c84c66d40cb0fd4be5202af19e
Joan Domingo <joands@gmail.com>
2015-01-04 15:41:34 +0100

    no message

--- app/src/main/java/cat/xojan/fittracker/session/DatePickerFragment.java
+++ app/src/main/java/cat/xojan/fittracker/session/DatePickerFragment.java
@@ -23,7 +23,7 @@ public class DatePickerFragment extends DialogFragment
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), R.style.AppTheme, this, year, month, day);
+        return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
