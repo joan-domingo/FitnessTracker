dc5fdfcaf6242cada3a8ea28dad41adc8c9b15cb
Joan Domingo <joands@gmail.com>
2015-01-11 00:16:53 +0100

    no message

--- app/src/main/java/cat/xojan/fittracker/googlefit/FitnessController.java
+++ app/src/main/java/cat/xojan/fittracker/googlefit/FitnessController.java
@@ -214,9 +214,9 @@ public class FitnessController {
                 mSingleSession = session;
                 mSingleSessionDataSets = dataSets;
                 // Process the data sets for this session
-                for (DataSet dataSet : dataSets) {
+                /*for (DataSet dataSet : dataSets) {
                     dumpDataSet(dataSet);
-                }
+                }*/
                 SessionFragment.getHandler().sendEmptyMessage(Constant.MESSAGE_SINGLE_SESSION_READ);
             }
 
