e50da9584033d823e05f1d3833d69827fa514ccc
Joan Domingo <joands@gmail.com>
2015-05-10 18:46:10 +0200

    add listview to start fragment

--- wear/src/main/java/cat/xojan/fittracker/fragment/StartFragment.java
+++ wear/src/main/java/cat/xojan/fittracker/fragment/StartFragment.java
@@ -3,17 +3,35 @@ package cat.xojan.fittracker.fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.wearable.view.WearableListView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import cat.xojan.fittracker.R;
 
-public class StartFragment extends Fragment {
+public class StartFragment extends Fragment implements WearableListView.OnClickListener {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.start_fragment, container, false);
+        View view = inflater.inflate(R.layout.start_fragment, container, false);
+        // Get the list component from the layout of the activity
+        WearableListView listView =
+                (WearableListView) view.findViewById(R.id.wearable_list);
+        listView.setGreedyTouchMode(true);
+
+        // Assign an adapter to the list
+        ListAdapter adapter = new ListAdapter(getActivity());
+        listView.setAdapter(adapter);
+
+        // Set a click listener
+        listView.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
